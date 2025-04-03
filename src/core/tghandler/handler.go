4275106870583139ae71be5c46868097b45b9415
FIZZI-77 <zaprudnovfedor@gmail.com>
2025-04-03 17:37:57 +0300

    add check len parts

--- src/core/tghandler/handler.go
+++ src/core/tghandler/handler.go
@@ -33,22 +33,22 @@ func (h *Handler) HandleMessage(bot *telego.Bot, message *telego.Message) (err e
 		h.checkCommand(parts[0], parts, amount)
 	}
 
-	switch parts[0] {
-	case "/start":
+	switch {
+	case parts[0] == "/start" && len(parts) == 1:
 		if err := h.sendStartMessage(bot, chatID); err != nil {
 			return err
 		}
 		h.addUser(username, telegramID)
-	case "/help":
+	case parts[0] == "/help" && len(parts) == 1:
 		if err := h.sendHelpMessage(bot, chatID); err != nil {
 			return err
 		}
-	case "/topup":
+	case parts[0] == "/topup" && len(parts) == 2:
 		h.topUpMoney(username, amount)
 
-	case "/send":
+	case parts[0] == "/send" && len(parts) == 3:
 		h.sendMoney(username, amount, parts[2])
-	case "/balance":
+	case parts[0] == "/balance" && len(parts) == 1:
 		h.takeBalanceMessage(bot, chatID, username)
 	default:
 		h.sendUnknownMessage(bot, chatID)
